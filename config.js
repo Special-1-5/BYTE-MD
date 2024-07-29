97937ab2a9d20521180cc5e1ed70df6a4a01fa65
Special-1-5 <specialsakala5@gmail.com>
2024-07-29 17:38:20 +0200

    Update config.js

--- config.js
+++ config.js
@@ -18,8 +18,8 @@ global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VaNRcHSJP219
 global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaNRcHSJP2199iMQ4W0l";
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://raw.githubusercontent.com/HyHamza/HyHamza/main/Images/logo.jpg";
 global.devs = "923072380380";
-global.sudo = process.env.SUDO || "923072380380";
-global.owner = process.env.OWNER_NUMBER || "923072380380";
+global.sudo = process.env.SUDO || "+260770418395";
+global.owner = process.env.OWNER_NUMBER || "+260770418395";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
@@ -57,7 +57,7 @@ global.SESSION_ID =
 
 
   
-  "Your session ID" //Make sure session id starts with Byte;;;
+  "Byte;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoib054R3Y0UXpqTGg2ZklyMGFsbWFPMUJORVJ2SS9tOGVKOEJ1bnBkNm8xRT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiL21VbUR4SllmWUpkSDhXZlc3ZzJUNWwrZlJhSHpTbHhVc0J3dXpMdjRIaz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIrT0tHOGFGQjA0dmo1bjJ4ejJpaFBQdXNXdU9EbmFlSVhJUk9PcHBxUVhzPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJUdVJYMFowVU1mQXlkUFpCVldXTTF3K1IreXFRQlFJdnZhMkhFazNiazNzPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IitJbHdIMTczNERETmZLTitiS1FhMmRVeHpBOVpTKy9WUVF3MzdNaTBibTg9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlllVzh2RHFhT1NIdVliTEpHQnhXei9QVjFQam9FY2ZoQkhJU0V5VXo2d2c9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMk5vZWoyZnZNc3ZRTjJCUzcxRVBSYWRWYlhWUGFsR1ZhNm14MG9CU0RrVT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiR1NkRjA4ZURIVjR4VnZoaHFqVFJXcHFGbVZtMmVkMVl2dDlqbjBSVUNVTT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InovTEIyeWVFa1d3aFNyTnJraWl3QXVkVzE0YStGczlYLzRaUXMvRS9FL3ZOb3hISFRyS2owNERJaUlYMVdsMkIwWWZzNjFHR282NVkzUE56T0V2SUJRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTAsImFkdlNlY3JldEtleSI6IkFuK3dCOGlnNEZSYlJIOUFwNFlUYmhtL1NnWWwxd1R3SkpwY2s0TFRWcm89IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6Ilk5WVk1bzNMUWJDdjU1WFJOSnNLQnciLCJwaG9uZUlkIjoiZjE2NzdjNDItZDhkOS00ZjZiLWIzMTQtODQwNjBjMDZjMzgxIiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlpWL3IvREtXRE5laUE1aDdieDF3eHN3ZU5CST0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJWWHlLak81QlMxZS9KZW10Z0RnYys3clBYSDQ9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiRlJXOEZWWjEiLCJtZSI6eyJpZCI6IjI2MDc3MDQxODM5NTo5QHMud2hhdHNhcHAubmV0In0sImFjY291bnQiOnsiZGV0YWlscyI6IkNKdkZqYWNDRUtUcG5yVUdHQUVnQUNnQSIsImFjY291bnRTaWduYXR1cmVLZXkiOiJYZXpwSTFUWURWTXlTWVdmODV6b0RyRzA4TFVLYTlIdFdoZ0VTcmFKc0JJPSIsImFjY291bnRTaWduYXR1cmUiOiJoMmNYaGI3VkdidXNrOCtVMmZmeStXOURxN3JJNFZHbElQMzBOY00xWm4rZW15ZmZSM0g3dnhoRHhJL3B4amUzYm5NT2VMR0NWeWttMnRKTEhIaWtDQT09IiwiZGV2aWNlU2lnbmF0dXJlIjoiYzFGUEdDYlZzc3kwVXlDeERCZjdMVi9vNlhTb2VCdytSSFo4Qis1RmRZY01UeTZlOCtyQnQ5TnVpM0FVZnVCd2RKTVo5YWZFcUpTNUUwZ080RTV2REE9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiIyNjA3NzA0MTgzOTU6OUBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJWM3M2U05VMkExVE1rbUZuL09jNkE2eHRQQzFDbXZSN1ZvWUJFcTJpYkFTIn19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzIyMjY2ODAyLCJteUFwcFN0YXRlS2V5SWQiOiJBQUFBQUpPbiJ9" //Make sure session id starts with Byte;;;
 
 
 
@@ -81,8 +81,8 @@ module.exports = {
   caption: process.env.CAPTION || "*Powered By TalkDrove*",
   author: process.env.PACK_AUTHER || "TalkDrove",
   packname: process.env.PACK_NAME || "♥️",
-  botname: process.env.BOT_NAME || "BYTE-MD",
-  ownername: process.env.OWNER_NAME || "TalkDrove",
+  botname: process.env.BOT_NAME || "REAPER-MD",
+  ownername: process.env.OWNER_NAME || "CHI-MAKU",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
@@ -94,7 +94,7 @@ module.exports = {
   aitts_Voice_Id: process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
   WORKTYPE: process.env.WORKTYPE || process.env.MODE || "public",
-  LANG: (process.env.THEME || "BYTE").toUpperCase(),
+  LANG: (process.env.THEME || "MEJA").toUpperCase(),
 };
 global.rank = "updated";
 global.isMongodb = false;
